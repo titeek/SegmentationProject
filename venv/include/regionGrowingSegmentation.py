37862484e1c5ef31b1ab78eac8b983d2c0b0c589
Krystian Bendinger <krystian.bendinger@gmail.com>
2020-05-09 04:07:25 +0200

    optimalizated algorythm of segmentation

--- venv/include/regionGrowingSegmentation.py
+++ venv/include/regionGrowingSegmentation.py
@@ -1,55 +1,52 @@
 import cv2 as cv
 import numpy as np
-
+import time
 
 tableOfSeeds = []
 difference = 0
+colorOfOutImage = 255
 
 
-def getCoordinates(x, y, shape):
+def getCoordinates(x, y, shape, outimg):
+    global colorOfOutImage
     outTable = []
-    maximumOfX = shape[1] - 1
-    maximumOfY = shape[0] - 1
+
+    valueXmin1 = (min(max(x - 1, 0), shape[1] - 1))
+    valueXplus1 = (min(max(x + 1, 0), shape[1] - 1))
+    valueYmin1 = (min(max(y - 1, 0), shape[0] - 1))
+    valueYplus1 = (min(max(y + 1, 0), shape[0] - 1))
 
     # left  top
-    coordinatesOfX = min(max(x - 1, 0), maximumOfX)
-    coordinatesOfY = min(max(y - 1, 0), maximumOfY)
-    outTable.append((coordinatesOfX, coordinatesOfY))
+    if outimg[valueXmin1, valueYmin1] != colorOfOutImage:
+        outTable.append((valueXmin1, valueYmin1))
 
     # left bot
-    coordinatesOfX = min(max(x - 1, 0), maximumOfX)
-    coordinatesOfY = min(max(y + 1, 0), maximumOfY)
-    outTable.append((coordinatesOfX, coordinatesOfY))
+    if outimg[valueXmin1, valueYplus1] != colorOfOutImage:
+        outTable.append((valueXmin1, valueYplus1))
 
     # left mid
-    coordinatesOfX = min(max(x - 1, 0), maximumOfX)
-    coordinatesOfY = y
-    outTable.append((coordinatesOfX, coordinatesOfY))
+    if outimg[valueXmin1, y] != colorOfOutImage:
+        outTable.append((valueXmin1, y))
 
     # center top
-    coordinatesOfX = x
-    coordinatesOfY = min(max(y - 1, 0), maximumOfY)
-    outTable.append((coordinatesOfX, coordinatesOfY))
+    if outimg[x, valueYmin1] != colorOfOutImage:
+        outTable.append((x, valueYmin1))
 
     # center bot
-    coordinatesOfX = x
-    coordinatesOfY = min(max(y + 1, 0), maximumOfY)
-    outTable.append((coordinatesOfX, coordinatesOfY))
+    if outimg[x, valueYplus1] != colorOfOutImage:
+        outTable.append((x, valueYplus1))
 
     # right top
-    coordinatesOfX = min(max(x + 1, 0), maximumOfX)
-    coordinatesOfY = min(max(y - 1, 0), maximumOfY)
-    outTable.append((coordinatesOfX, coordinatesOfY))
+    if outimg[valueXplus1, valueYmin1] != colorOfOutImage:
+        outTable.append((valueXplus1, valueYmin1))
 
     # right mid
-    coordinatesOfX = min(max(x + 1, 0), maximumOfX)
-    coordinatesOfY = y
-    outTable.append((coordinatesOfX, coordinatesOfY))
+    if outimg[valueXplus1, y] != colorOfOutImage:
+        outTable.append((valueXplus1, y))
 
     # right bot
-    coordinatesOfX = min(max(x + 1, 0), maximumOfX)
-    coordinatesOfY = min(max(y + 1, 0), maximumOfY)
-    outTable.append((coordinatesOfX, coordinatesOfY))
+    if outimg[valueXplus1, valueYplus1] != colorOfOutImage:
+        outTable.append((valueXplus1, valueYplus1))
 
     return outTable
 
@@ -62,31 +59,35 @@ def mouseClicked(event, x, y, flags, params):
 
 def regionGrowing(image, startPoint):
     global difference
+    global colorOfOutImage
+    start = time.time()
     tempList = []
     outImage = np.zeros_like(image)
     tempList.append((startPoint[0], startPoint[1]))
     processed = []
     bright = image[startPoint[0], startPoint[1]]
-    print("Base brightness: " + str(bright))
+    # print("Base brightness: " + str(bright))
     while len(tempList) > 0:
         pixel = tempList[0]
-        outImage[pixel[0], pixel[1]] = 255  #color of base pixel
-        for coord in getCoordinates(pixel[0], pixel[1], image.shape):
-            if (image[coord[0], coord[1]] >= bright - difference) & (image[coord[0], coord[1]] <= bright + difference):
-                print(image[coord[0], coord[1]]) #show brightness of pixel
-                outImage[coord[0], coord[1]] = 255 #set brigthness for out image
-                if not coord in processed:
-                    tempList.append(coord)
+        for coord in getCoordinates(pixel[0], pixel[1], image.shape, outImage):
+            if coord not in processed and bright + difference >= image[coord[0], coord[1]] >= bright - difference:
+                tempList.append(coord)
+                outImage[coord[0], coord[1]] = colorOfOutImage  # set brigthness for out image
                 processed.append(coord)
+                # print(image[coord[0], coord[1]])  # show brightness of pixel
+
         tempList.pop(0)
-        cv.imshow("Progress window", outImage)
-        cv.waitKey(1)
+        # cv.imshow("Progress window", outImage)
+        # cv.waitKey(1)
+
+    stop = time.time()
+    t = stop - start
+    print("Time: " + str(t))
 
     return outImage
 
 
 def main(imageToSeg, x, y, use, differenceP):
-
     image = cv.imread(imageToSeg, 0)
     cv.namedWindow('Initial segmentation')
 
@@ -98,10 +99,6 @@ def main(imageToSeg, x, y, use, differenceP):
 
     global difference
     difference = 256 * differenceP * 0.01
-    print("Use:")
-    print(use)
-    print("DiffP:")
-    print(differenceP)
     cv.imshow('Initial segmentation', image)
     cv.waitKey()
     startingPoint = tableOfSeeds[-1]
